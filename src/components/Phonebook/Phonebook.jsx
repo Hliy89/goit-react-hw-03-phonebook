d32332e50b9f95ca4a2968d00268b012d4bfa6b2
Yaroslav Hliy <hliy.yaroslav@gmail.com>
2021-04-26 14:30:32 +0300

    изменили исходный массив

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -10,12 +10,7 @@ import style from './Phonebook.module.css';
 
 class Phonebook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Daenerys Targaryen', number: '459-12-56' },
-      { id: 'id-2', name: 'Jaime Lannister', number: '443-89-12' },
-      { id: 'id-3', name: 'Jon Snow', number: '645-17-79' },
-      { id: 'id-4', name: 'Tyrion Lannister', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
